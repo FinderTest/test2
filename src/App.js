0f0cac1871e71287acb4cdd4e545aa885d048f70
Chaynikz <finder17@mail.ru>
2020-12-02 11:13:27 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Тестовый репозиторий для освоения github
+                </p>
+
+            </header>
+        </div>
+    );
+}
+
+export default App;
